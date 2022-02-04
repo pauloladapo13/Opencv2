47fb4fc6037667e135732dd08234df1feccac8b4
Paul <pauloladapo13@gmail.com>
2022-02-04 00:28:08 +0000

    "Open an Image"

---
+++ CMakeFiles/opencv2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv2.dir/main.cpp.o"
+  "CMakeFiles/opencv2.dir/main.cpp.o.d"
+  "opencv2"
+  "opencv2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/opencv2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for opencv2.
+# This may be replaced when dependencies are built.
